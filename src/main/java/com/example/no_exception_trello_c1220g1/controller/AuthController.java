c4d3e01a9c331b62d26e72019c7969c92faada79
LucHoang <hoangsyluc@gmail.com>
2021-06-11 23:01:27 +0700

    validate register

--- src/main/java/com/example/no_exception_trello_c1220g1/controller/AuthController.java
+++ src/main/java/com/example/no_exception_trello_c1220g1/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.no_exception_trello_c1220g1.controller;
 
 import com.example.no_exception_trello_c1220g1.model.Entity.User;
 import com.example.no_exception_trello_c1220g1.model.dto.JwtResponse;
+import com.example.no_exception_trello_c1220g1.model.dto.UserDto;
 import com.example.no_exception_trello_c1220g1.service.JwtService;
 import com.example.no_exception_trello_c1220g1.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -42,7 +46,19 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    public ResponseEntity<User> register(@Valid @RequestBody UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (userService.checkUserNameEmail(userDto.getUserName(), userDto.getEmail())) {
+            User user = User.builder()
+                    .userName(userDto.getUserName())
+                    .passWord(userDto.getPassWord())
+                    .email(userDto.getEmail())
+                    .phone(userDto.getPhone())
+                    .build();
+            return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/no_exception_trello_c1220g1/model/Entity/User.java
+++ src/main/java/com/example/no_exception_trello_c1220g1/model/Entity/User.java
@@ -1,16 +1,22 @@
 package com.example.no_exception_trello_c1220g1.model.Entity;
 
-import lombok.Data;
+import lombok.*;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
+//    @Size(min = 6, max = 32)
     private String passWord;
     private String avatar;
     private String email;
