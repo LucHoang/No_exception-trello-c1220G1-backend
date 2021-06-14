de6f80956735f1ec6b848839b623f18159d4c7fa
LucHoang <hoangsyluc@gmail.com>
2021-06-14 11:34:10 +0700

    fix loi

--- src/main/java/com/example/no_exception_trello_c1220g1/repository/ICardRepository.java
+++ src/main/java/com/example/no_exception_trello_c1220g1/repository/ICardRepository.java
@@ -1,8 +1,9 @@
 package com.example.no_exception_trello_c1220g1.repository;
 
+import com.example.no_exception_trello_c1220g1.model.Entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICardRepositoryRepository extends JpaRepository<ICardRepositoryRepository,Long> {
+public interface ICardRepository extends JpaRepository<Card,Long> {
 }
