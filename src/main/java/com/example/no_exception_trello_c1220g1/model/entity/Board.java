14eca77cf51ad8a5c6448bd2e7cbcf73ac93129c
LucHoang <hoangsyluc@gmail.com>
2021-06-17 16:47:32 +0700

    config deploy heroku

--- src/main/java/com/example/no_exception_trello_c1220g1/model/entity/Board.java
+++ src/main/java/com/example/no_exception_trello_c1220g1/model/entity/Board.java
@@ -1,12 +1,11 @@
 package com.example.no_exception_trello_c1220g1.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
+@EqualsAndHashCode(callSuper=true)
 @Entity
 @Data
 @NoArgsConstructor
--- src/main/java/com/example/no_exception_trello_c1220g1/model/entity/CardTagUser.java
+++ src/main/java/com/example/no_exception_trello_c1220g1/model/entity/CardTagUser.java
@@ -2,9 +2,11 @@ package com.example.no_exception_trello_c1220g1.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
+@EqualsAndHashCode(callSuper=true)
 @Entity
 @Data
 @NoArgsConstructor
